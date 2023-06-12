088b252651a4523826d6fd64b56f7d0c63761f00
FaunuX <judahpspiegel@gmail.com>
2023-06-12 10:23:08 -0700

    add color inputs

--- chromahackerweb/__main__.py
+++ chromahackerweb/__main__.py
@@ -1,19 +1,19 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 from chromahacker.palettize import palettize
 
 app = Flask(__name__)
 
 @app.route('/palettize', methods=['GET'])
 def palettize_endpoint():
-    data = request.get_json()
-    url = data.get('url')
-    args = data.get('args', [])
+    url, arg0, arg1, arg2 = request.args.get('url'), request.args.get('arg0'), request.args.get('arg1'), request.args.get('arg2')
 
-    print(url)
-    print(args)
-    palettize(url, *args)
+    palettize(url, arg0, arg1, arg2)
 
     return send_file('../wallpaper.jpg', mimetype='image/jpg')
+
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
